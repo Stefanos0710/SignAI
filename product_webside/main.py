8494c065110ee84827a516d07a33236ecad8fa4f
Stefanos <165141357+Stefanos0710@users.noreply.github.com>
2025-10-15 20:34:26 +0200

    Add basic styling and navigation to homepage
    Introduced a main CSS file for consistent styling, added navigation and content sections to index.html, and updated Flask app configuration for development with template auto-reload and debug mode. Includes cache-control meta tags for development; these should be removed in production.

--- product_webside/main.py
+++ product_webside/main.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask import request, render_template, url_for, redirect
 
+# Initialisiere die Flask-Anwendung
 app = Flask(__name__)
 
+# DELETE THIS IN PRODUKTION
+app.config.update(
+    DEBUG=True,
+    TEMPLATES_AUTO_RELOAD=True
+)
+
+# main route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# start the server
 if __name__ == '__main__':
     app.run(debug=True)
