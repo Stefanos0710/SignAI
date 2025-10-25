122fe77a76a5ca24e2d1e82651e2ac3242d56f3b
Stefanos <165141357+CodeProTech@users.noreply.github.com>
2025-10-25 20:38:12 +0200

    Refactor Updater and add temp file management
    Removed commented legacy Updater class and added new methods for unzipping updates, setting up new versions, deleting temporary files, and restoring temporary data. Introduced tmp_folders and improved backup and restore logic for user data.

--- app/updater/updater.py
+++ app/updater/updater.py
@@ -24,208 +24,6 @@ C:\Program Files (x86)\SignAI - Desktop
 
 """
 
-# class Updater:
-#     def __init__(self):
-#         # Load environment variables from a .env file
-#         load_dotenv()
-#         self.API_KEY = os.getenv("GITHUB_TOKEN")
-#         self.OWNER = os.getenv("REPO_OWNER")
-#         self.REPO = os.getenv("REPO_NAME")
-#
-#         # make url and headers for api request
-#         self.API_URL = f"https://api.github.com/repos/{self.OWNER}/{self.REPO}/releases/latest"
-#         self.HEADERS = {"Authorization": f"token {self.API_KEY}"}
-#
-#         # files and folders to save during update
-#         self.save_files = ["settings", "videos/", "data/"]
-#
-#         # main paths
-#         self.USER_HOME = Path.home()
-#         self.BACKUP_DIR = self.USER_HOME / "AppData" / "Local" / "SignAI" / "backup"
-#         self.UPDATE_DIR = self.USER_HOME / "AppData" / "Local" / "SignAI" / "update_tmp"
-#         self.UPDATE_FILE = self.UPDATE_DIR / "update.zip"
-#
-#     def start(self):
-#         """ Here is the logic to start the update process """
-#         print("-- Staring update --")
-#
-#         # save user data
-#         self.save_user_data()
-#         print("User data saved.")
-#
-#         # get current version and print it
-#         current_version = self.get_current_version()
-#         print(f"Current version: {current_version}")
-#
-#         # check for updates
-#         download_url, latest_version = self.check_for_updates(current_version)
-#         if not download_url:
-#             print("No updates available.")
-#             return
-#         print(f"Update available: {latest_version}")
-#
-#         zip_path = self.load_new_version(download_url)
-#         if not zip_path:
-#             print("Failed to download the update.")
-#             return
-#
-#         self.unzip_new_version()
-#         self.replace_old_version()
-#         self.get_user_data()
-#
-#         print("-- Finished update! --")
-#
-#     def get_current_version(self):
-#         """Get the local version from version.txt file"""
-#         version_file = Path("version.txt")
-#         if version_file.exists():
-#             with open(version_file, "r") as f:
-#                 return f.read().strip()
-#         else:
-#             print("No version file.")
-#             return "0.0.0"
-#
-#     def check_for_updates(self, current_version):
-#         """Check for updates by comparing the current version with the latest release on GitHub and get the download URL if an update is available."""
-#         response = requests.get(self.API_URL, headers=self.HEADERS)
-#         print("Checking for updates...")
-#
-#         # check id response failed
-#         if response.status_code != 200:
-#             print("Failed to fetch release information.")
-#             return None
-#
-#         data = response.json()
-#         assets = data.get("assets", [])
-#
-#         # check if there are assets in the release
-#         if not assets:
-#             print("No assets found in the latest release.")
-#             return None
-#
-#         for asset in assets:
-#             name = asset.get("name", "")
-#             if name.endswith(".zip"):
-#                 print(f"Found a zip asset: {name}")
-#                 return asset["browser_download_url"]
-#
-#         print("No assets found in the latest release.")
-#         return None
-#
-#     def load_new_version(self, download_url):
-#         """Download the new version from the provided download URL and save it in SignAI/update_tmp."""
-#         USER_HOME = Path.home()
-#         UPDATE_DIR = USER_HOME / "AppData" / "Local" / "SignAI" / "update_tmp"
-#         UPDATE_FILE = UPDATE_DIR / "update.zip"
-#
-#         # create or clean update directory
-#         if UPDATE_DIR.exists():
-#             shutil.rmtree(UPDATE_DIR)
-#         UPDATE_DIR.mkdir(parents=True, exist_ok=True)
-#
-#         print("Starting download of the new version...")
-#
-#         # download the file
-#         with requests.get(download_url, headers=self.HEADERS, stream=True) as response:
-#             if response.status_code != 200:
-#                 print("Failed to download update file.")
-#                 return None
-#
-#             total = int(response.headers.get("content-length", 0))
-#             downloaded = 0
-#
-#             with open(UPDATE_FILE, "wb") as f:
-#                 for chunk in response.iter_content(1024):
-#                     if chunk:
-#                         f.write(chunk)
-#                         downloaded += len(chunk)
-#                         # show progress in terminal - should may make later a progress bar in the GUI
-#                         print(f"\rProgress: {downloaded / total:.0%}", end="")
-#
-#         print("\n Download complete!")
-#         return UPDATE_FILE
-#
-#     def unzip_new_version(self):
-#         """Unzip the downloaded update file into the update_tmp directory."""
-#         print("Unzipping the new version...")
-#         try:
-#             with zipfile.ZipFile(self.UPDATE_FILE, 'r') as zip_ref:
-#                 zip_ref.extractall(self.UPDATE_DIR)
-#             print("Unzipping complete!")
-#         except zipfile.BadZipFile as e:
-#             print(f"Failed to unzip the update file: {e}")
-#
-#     def replace_old_version(self):
-#         """Replace the old version files with the new version files."""
-#         print("replacing ikd version files...")
-#         try:
-#             app_dir = Path.cwd()
-#
-#             for item in self.UPDATE_DIR.iterdir():
-#                 target_path = app_dir / item.name
-#                 if target_path.exists():
-#                     if target_path.is_file():
-#                         target_path.unlink()
-#                     elif target_path.is_dir():
-#                         shutil.rmtree(target_path)
-#
-#                 if item.is_file():
-#                     shutil.copy2(item, target_path)
-#                 elif item.is_dir():
-#                     shutil.copytree(item, target_path)
-#                 print(f"Replaced: {item.name}")
-#             print("Replacement complete!")
-#         except Exception as e:
-#             print(f"Failed to replace old version files: {e}")
-#
-#     def save_user_data(self):
-#         """Save users data before updating.
-#         self.save_files = ["settings", "videos/", "data/"]
-#         """
-#
-#         USER_HOME = Path.home()
-#         BACKUP_DIR = USER_HOME / "AppData" / "Local" / "SignAI" / "backup"
-#
-#         # check if backup directory exists, if not create it
-#         if BACKUP_DIR.exists():
-#             shutil.rmtree(BACKUP_DIR) # delete existing backup
-#         BACKUP_DIR.mkdir(parents=True, exist_ok=True)
-#
-#         for item in self.save_files:
-#             if os.path.exists(item):
-#                 source_path = Path(item)
-#                 destination_path = BACKUP_DIR / source_path.name
-#                 if source_path.is_file():
-#                     shutil.copy2(source_path, destination_path)
-#                 elif source_path.is_dir():
-#                     shutil.copytree(source_path, destination_path)
-#                 print(f"Saved: {item}")
-#             else:
-#                 print(f"{item} does not exist, skipping backup.")
-#
-#     def get_user_data(self):
-#         """Retrieve users data after updating.
-#         self.save_files = ["settings", "videos/", "data/"]
-#         """
-#
-#         USER_HOME = Path.home()
-#         BACKUP_DIR = USER_HOME / "AppData" / "Local" / "SignAI" / "backup"
-#
-#         if not BACKUP_DIR.exists():
-#             """ Implement here logic to create "fake backup" or just handle it """
-#             print("No backups found!")
-#
-#         for item in self.save_files:
-#             backup_item = BACKUP_DIR / Path(item).name
-#             if backup_item.exists():
-#                 if backup_item.is_file():
-#                     shutil.copy2(backup_item, item)
-#                 elif backup_item.is_dir():
-#                     if os.path.exists(item):
-#                         shutil.rmtree(item)
-#                     shutil.copytree(backup_item, item)
-#                 print(f"{item} restored successfully.")
-
 
 class Updater:
     def __init__(self):
@@ -242,6 +40,9 @@ class Updater:
         # files and folders to save during update
         self.save_files = ["settings/", "videos/", "data/"]
         self.dont_delete = ["tmp_data/", "tmp_updater/", "Uninstall SignAI - Desktop_lang.ifl", "Uninstall SignAI - Desktop.exe", "Uninstall SignAI - Desktop.dat"]
+
+        # tmp folders
+        self.tmp_folders = ["tmp_data/", "tmp_updater/", "tmp_version/"]
 
     def check_for_updates(self, current_version):
         print("Checking for updates...")
@@ -400,6 +201,64 @@ class Updater:
             print(f"Updated version file to: {new_version}")
         except Exception as e:
             print(f"Failed to update version file: {e}")
+
+    def unzip_new_version(self, zip_path):
+        zip_path = Path(zip_path)
+        extract_to = zip_path.parent # in the same dic as zip file
+
+        extract_to.mkdir(parents=True, exist_ok=True)
+
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.extractall(extract_to)
+
+        # del zip file
+        zip_path.unlink()
+
+        print(f"Unzipped '{zip_path.name}' into '{extract_to}'")
+
+    def setup_new_version(self, new_version):
+        new_version = Path(new_version)
+        app_dir = self.get_project_paths()
+
+        for item in new_version.iterdir():  # gets throuh all files and folder in the tmp_version
+            target = new_version / item.name
+            if item.is_dir():
+                shutil.copytree(item, target, dirs_exist_ok=True)  # copy dictory with all content
+            else:
+                shutil.copy2(item, target)  # copy file with all content
+
+    def delete_tmp_files(self):
+        app_dir = self.get_project_paths()
+
+        for folder in self.tmp_folders:
+            tmp_path = app_dir / folder
+            if tmp_path.exists():
+                shutil.rmtree(tmp_path)
+                print(f"Deleted temporary folder: {folder}")
+            else:
+                print(f"Temporary folder not found, skipped: {folder}")
+
+    def get_tmp_data(self):
+        app_dir = self.get_project_paths()
+
+        tmp_folders = {
+            "tmp_settings": "settings",
+            "tmp_videos": "videos",
+            "tmp_data": "data"
+        }
+
+        for tmp_name, target_name in tmp_folders.items():
+            source = app_dir / tmp_name
+            target = app_dir / target_name
+
+            if source.exists():
+                # Ordner im App-Verzeichnis vorher löschen, falls er existiert
+                if target.exists():
+                    shutil.rmtree(target)
+                shutil.copytree(source, target)
+                print(f"Restored '{target_name}' from '{tmp_name}'")
+            else:
+                print(f"Backup folder '{tmp_name}' not found, skipping.")
 
     def start(self):
         if self.check_for_updates(self.current_version()):
